d26595794d8d2369f7aa51cdabb0d78e3e9f5f0c
Aniket Sanjay Suryavanshi <aniketsuryavanshi597@gmail.com>
2025-07-13 22:30:52 +0530

    Partition of Elements Positive & Negative  In an Array

--- src/Arrays/PositiveNegativePartition.java
+++ src/Arrays/PositiveNegativePartition.java
@@ -4,53 +4,29 @@ import java.util.Arrays;
 
 public class PositiveNegativePartition {
 	
-	public static int[] ans2(int[] a)
+	public static int[] partition(int[] nums)
 	{
-		int i = 0, j = a.length-1;
-		
-		
-		while(i<=j)
-		{
-			
-			if (a[i] > 0 && a[j] < 0 ){
-				i++;
-				j--;
-			}
-			if(a[i]<0 && a[j]<0) j--;
-			
-			if(a[i]>0 && a[j]>0) i++;
-			
-			if(a[i]<0 && a[j]<0){
-				int temp = a[i];
-				a[i] = a[j];
-				a[j] = temp;
-			}
+		int i = 0, j = nums.length-1;
+		while(nums[i]<0){
+			i++;
 		}
-		return a;
+		while (nums[j]>0) {
+			j--;
+		}
+		if (i<j) {
+			int temp = nums[i];
+			nums[i] = nums[j];
+			nums[j] = temp;
+		}
+		return nums;
 	}
 	
-	public static int[] ans(int[] a)
-	{
-		int n = a.length;
-		for (int i = 0; i < n; i++) {
-			if (a[i]<0) {
-				for (int j = i+1; j < a.length; j++) {
-					if (a[j]>0) {
-						int temp = a[i];
-						a[i] = a[j];
-						a[j] = temp;
-					}
-				}
-			}
-		}
-		return a;
-	}
 	
 	public static void main(String[] args)
 	{
 		int arr[] = {-1,-2,3,4,-5,6,-7};
-		System.out.println(Arrays.toString(ans(arr)));
-		System.out.println(Arrays.toString(ans2(arr)));
+		
+		System.out.println(Arrays.toString(partition(arr)));
 	}
 
 }
