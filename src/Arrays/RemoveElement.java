0fac790daf694e6424dda916f144748c0e6bd4dc
Aniket Sanjay Suryavanshi <aniketsuryavanshi597@gmail.com>
2025-07-14 12:23:25 +0530

    RemoveElement from Array

---
+++ src/Arrays/RemoveElement.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class RemoveElement {
+
+	public static int[] remove(int[] nums,int elemnt)
+	{
+		int[] ans = new int[nums.length-1];
+		int j =0;
+		boolean flag = false;
+		while(j<nums.length) 
+		{
+			if(nums[j]==elemnt)
+			{
+				flag =true;
+				j++;
+			}
+			if(flag && j<nums.length) 
+				ans[j-1] = nums[j];
+			else if(j<nums.length)  
+				ans[j] = nums[j];
+			j++;
+		}
+		return ans;
+	}
+	public static void main(String[] args) {
+		int[] nums = {2,3,6,8,9,10};
+		int elemnt = 3;
+		System.out.println(Arrays.toString(remove(nums, elemnt)));
+	}
+ }
